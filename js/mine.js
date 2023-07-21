3cc47cc69b5ebb1402516d5cbb32557bae5e6f0a
AhmedAshour683 <136949562+AhmedAshour683@users.noreply.github.com>
2023-07-21 20:33:43 +0300

    Add files via upload

---
+++ js/mine.js
@@ -0,0 +1,111 @@
+var siteName = document.getElementById("siteName")
+var siteUrl = document.getElementById("siteUrl")
+
+var container=[];
+
+if(  localStorage.getItem("webSites") == null )
+{
+    container = [] ;
+}
+else
+{
+      container =  JSON.parse(localStorage.getItem("webSites")) ;
+
+      display()
+}
+
+var regex = /^(https)/
+
+function addRow()
+{
+if(regex.test(siteUrl.value))
+
+{
+
+    var oneSite = {
+        theSiteName: siteName.value,
+        theSiteUrl: siteUrl.value
+    }
+
+    var cycle=""
+    if(  localStorage.getItem("webSites") !== null ) {
+
+        
+        
+
+        for( var b=0;b<container.length;b++){
+
+            cycle=(JSON.parse(localStorage.getItem("webSites")))[b].theSiteUrl==oneSite.theSiteUrl 
+            break
+            }   
+            
+    }
+    
+
+        else{
+           
+            cycle=false
+            console.log(localStorage.getItem("webSites") )
+            console.log(cycle)
+            console.log("hi from null")
+           
+        }
+    
+
+     if(cycle==false){
+    container.push(oneSite)
+    localStorage.setItem("webSites",JSON.stringify(container))
+
+    clear()
+    display()
+    }
+
+    else
+    {
+        window.alert("this Site Already Stored")
+    }
+
+}
+
+else
+{
+    window.alert("!! URL Must Start with https")
+}
+
+}
+
+function clear(){
+    siteName.value=""
+    siteUrl.value=""
+}
+
+
+
+
+
+function display()
+{
+    var getter =""
+
+for (var i=0 ;i<container.length;i++) {
+
+
+    getter+=`<tr>
+    <td>${i}</td>
+    <td>${container[i].theSiteName}</td>
+    <td><button class="btn btn-success px-4"><a href="${container[i].theSiteUrl}" class="text-decoration-none text-white" target="_blank">Visit</a></button></td>
+    <td><button onclick="deleteElement(${i})" class="btn btn-warning">Delete</button></td>
+</tr>
+`
+}
+document.getElementById("tbody").innerHTML=getter
+}
+
+
+function deleteElement(Pindex){
+    container.splice(Pindex,1)
+    localStorage.setItem("webSites",JSON.stringify(container))
+    display()
+
+}
+
